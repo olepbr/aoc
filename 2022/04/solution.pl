99294f7b04c3c31214cf9ba647108264814ad15b
Ole Peder Brandtzæg <olepbr@gmail.com>
2022-12-08 11:08:13 +0100

    2022/04: use size instead of members, add comments

--- 2022/04/solution.pl
+++ 2022/04/solution.pl
@@ -7,11 +7,14 @@ use utf8;
 use File::Slurper qw(read_lines);
 use Set::Object qw(set);
 
+# array of lines -> split on , and - and make arrayref -> construct tuples of sets and put them into an array
 my @tuples = map { [ (set($_->[0]..$_->[1]), set($_->[2]..$_->[3])) ] } map { [ split(/-|,/, $_) ] } read_lines("input.txt");
 
+# grep for tuples where either is subset of the other and count
 my $fully_contained = scalar grep { $_->[0] <= $_->[1] || $_->[1] <= $_->[0] } @tuples;
 
-my $overlapping = scalar grep { scalar ($_->[0] * $_->[1])->members } @tuples;
+# grep for tuples where intersection is > 0 and count
+my $overlapping = scalar grep { ($_->[0] * $_->[1])->size } @tuples;
 
 say "Among the pairs, $fully_contained ranges fully contain the other.";
 
