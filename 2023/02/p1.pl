36177d3354e4047beebade8f05fb2f4c3960e014
Ole Peder Brandtzæg <olebra@samfundet.no>
2023-12-03 15:30:52 +0100

    2023: format with perl navigator

--- 2023/02/p1.pl
+++ 2023/02/p1.pl
@@ -1,32 +1,35 @@
 #! /usr/bin/perl
 
 use v5.36;
-use File::Slurper qw(read_lines);
-use List::Util qw(any);
+use File::Slurper qw( read_lines );
+use List::Util    qw( any );
 
-my $max_red = 12;
+my $max_red   = 12;
 my $max_green = 13;
-my $max_blue = 14;
+my $max_blue  = 14;
 
 my @test_lines = (
-"Game 1: 3 blue, 4 red; 1 red, 2 green, 6 blue; 2 green",
-"Game 2: 1 blue, 2 green; 3 green, 4 blue, 1 red; 1 green, 1 blue",
-"Game 3: 8 green, 6 blue, 20 red; 5 blue, 4 red, 13 green; 5 green, 1 red",
-"Game 4: 1 green, 3 red, 6 blue; 3 green, 6 red; 3 green, 15 blue, 14 red",
-"Game 5: 6 red, 1 blue, 3 green; 2 blue, 1 red, 2 green",
+    "Game 1: 3 blue, 4 red; 1 red, 2 green, 6 blue; 2 green",
+    "Game 2: 1 blue, 2 green; 3 green, 4 blue, 1 red; 1 green, 1 blue",
+    "Game 3: 8 green, 6 blue, 20 red; 5 blue, 4 red, 13 green; 5 green, 1 red",
+    "Game 4: 1 green, 3 red, 6 blue; 3 green, 6 red; 3 green, 15 blue, 14 red",
+    "Game 5: 6 red, 1 blue, 3 green; 2 blue, 1 red, 2 green",
 );
 
 my $id_sum = 0;
 
 # The subsets are a red herring; we only really care about finding a number of
 # (red|green|blue) higher than allowed somewhere in the game.
-for my $line (read_lines("input.txt")) {
-	my ($game_id, $rest) = $line =~ /Game (\d+): (.*)/;
-	my @reds = $rest =~ /(\d+) red/g;
-	my @blues = $rest =~ /(\d+) blue/g;
-	my @greens = $rest =~ /(\d+) green/g;
-	next if (any { $_ > $max_red } @reds) || (any { $_ > $max_green } @greens) || (any { $_ > $max_blue } @blues);
-	$id_sum += $game_id;
+for my $line ( read_lines("input.txt") ) {
+    my ( $game_id, $rest ) = $line =~ /Game (\d+): (.*)/;
+    my @reds   = $rest =~ /(\d+) red/g;
+    my @blues  = $rest =~ /(\d+) blue/g;
+    my @greens = $rest =~ /(\d+) green/g;
+    next
+      if ( any { $_ > $max_red } @reds )
+      || ( any { $_ > $max_green } @greens )
+      || ( any { $_ > $max_blue } @blues );
+    $id_sum += $game_id;
 }
 
 say $id_sum;
--- 2023/02/p2.pl
+++ 2023/02/p2.pl
@@ -1,25 +1,25 @@
 #! /usr/bin/perl
 
 use v5.36;
-use File::Slurper qw(read_lines);
-use List::Util qw(max);
+use File::Slurper qw( read_lines );
+use List::Util    qw( max );
 
 my @test_lines = (
-"Game 1: 3 blue, 4 red; 1 red, 2 green, 6 blue; 2 green",
-"Game 2: 1 blue, 2 green; 3 green, 4 blue, 1 red; 1 green, 1 blue",
-"Game 3: 8 green, 6 blue, 20 red; 5 blue, 4 red, 13 green; 5 green, 1 red",
-"Game 4: 1 green, 3 red, 6 blue; 3 green, 6 red; 3 green, 15 blue, 14 red",
-"Game 5: 6 red, 1 blue, 3 green; 2 blue, 1 red, 2 green",
+    "Game 1: 3 blue, 4 red; 1 red, 2 green, 6 blue; 2 green",
+    "Game 2: 1 blue, 2 green; 3 green, 4 blue, 1 red; 1 green, 1 blue",
+    "Game 3: 8 green, 6 blue, 20 red; 5 blue, 4 red, 13 green; 5 green, 1 red",
+    "Game 4: 1 green, 3 red, 6 blue; 3 green, 6 red; 3 green, 15 blue, 14 red",
+    "Game 5: 6 red, 1 blue, 3 green; 2 blue, 1 red, 2 green",
 );
 
 # grep for number of reds, greens and blues. take max of each list, which will
 # be the minimum number needed.
 
 my $power_sum = 0;
-for my $line (read_lines("input.txt")) {
-	my $min_reds = max $line =~ /(\d+) red/g;
-	my $min_blues = max $line =~ /(\d+) blue/g;
-	my $min_greens = max $line =~ /(\d+) green/g;
-	$power_sum += $min_reds * $min_greens * $min_blues;
+for my $line ( read_lines("input.txt") ) {
+    my $min_reds   = max $line =~ /(\d+) red/g;
+    my $min_blues  = max $line =~ /(\d+) blue/g;
+    my $min_greens = max $line =~ /(\d+) green/g;
+    $power_sum += $min_reds * $min_greens * $min_blues;
 }
 say $power_sum;
